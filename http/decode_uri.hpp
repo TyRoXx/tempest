5e656e8cbdbacb650818f803c3281c9efd916f38
TyRoXx <tyroxxxx@gmail.com>
2013-06-08 14:26:11 +0200

    url decoding (%20 etc)

---
+++ http/decode_uri.hpp
@@ -0,0 +1,76 @@
+#ifndef TEMPEST_HTTP_DECODE_URI_HPP
+#define TEMPEST_HTTP_DECODE_URI_HPP
+
+
+#include <iterator>
+#include <stdexcept>
+
+
+namespace tempest
+{
+	namespace detail
+	{
+		template <class InputIterator>
+		typename std::iterator_traits<InputIterator>::value_type
+		require_char(InputIterator &position,
+		             InputIterator end)
+		{
+			if (position == end)
+			{
+				throw std::invalid_argument("Unexpected end of input");
+			}
+
+			return *position++;
+		}
+
+		template <class Char>
+		unsigned decode_hex(Char c)
+		{
+			if (c >= '0' && c <= '9')
+			{
+				return (c - '0');
+			}
+
+			c = std::tolower(c);
+			if (c >= 'a' && c <= 'f')
+			{
+				return (c - 'a' + 10);
+			}
+
+			throw std::invalid_argument("Hexadecimal digit expected");
+		}
+	}
+
+	template <class InputIterator, class OutputIterator>
+	OutputIterator
+	decode_uri(InputIterator source_begin,
+	           InputIterator source_end,
+	           OutputIterator dest)
+	{
+		while (source_begin != source_end)
+		{
+			auto const current = *source_begin++;
+			if (current == '%')
+			{
+				unsigned char_value = 0;
+				for (unsigned i = 0; i < 2; ++i)
+				{
+					auto const digit = detail::require_char(source_begin, source_end);
+					unsigned digit_value = detail::decode_hex(digit);
+					char_value *= 16;
+					char_value += digit_value;
+				}
+
+				*dest++ = static_cast<decltype(current)>(char_value);
+			}
+			else
+			{
+				*dest++ = current;
+			}
+		}
+
+		return dest;
+	}
+}
+
+#endif
--- test/http.cpp
+++ test/http.cpp
@@ -1,6 +1,7 @@
 #include <boost/test/unit_test.hpp>
 #include "http/http_request.hpp"
 #include "http/http_response.hpp"
+#include "http/decode_uri.hpp"
 
 BOOST_AUTO_TEST_CASE(http_request_parse)
 {
@@ -37,4 +38,39 @@ BOOST_AUTO_TEST_CASE(http_response_print)
 	                  "\r\n"
 	                  "404"
 	                  );
+}
+
+namespace
+{
+	bool test_decode_uri(std::string const &encoded,
+	                     std::string const &raw)
+	{
+		std::string decoded;
+		tempest::decode_uri(encoded.begin(),
+		                    encoded.end(),
+		                    std::back_inserter(decoded));
+		return (raw == decoded);
+	}
+
+	void decode_invalid(std::string const &invalid)
+	{
+		std::string decoded;
+		tempest::decode_uri(invalid.begin(),
+		                    invalid.end(),
+		                    std::back_inserter(decoded));
+	}
+}
+
+BOOST_AUTO_TEST_CASE(http_decode_uri)
+{
+	BOOST_CHECK(test_decode_uri("", ""));
+	BOOST_CHECK(test_decode_uri("abc", "abc"));
+	BOOST_CHECK(test_decode_uri("%20", " "));
+	BOOST_CHECK(test_decode_uri("a%202", "a 2"));
+
+	BOOST_CHECK_THROW(decode_invalid("%"), std::invalid_argument);
+	BOOST_CHECK_THROW(decode_invalid("%1"), std::invalid_argument);
+	BOOST_CHECK_THROW(decode_invalid("%%"), std::invalid_argument);
+	BOOST_CHECK_THROW(decode_invalid("%2G"), std::invalid_argument);
+	BOOST_CHECK_THROW(decode_invalid("%H1"), std::invalid_argument);
 }
