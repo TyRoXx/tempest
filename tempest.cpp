f42e6235a3bd94c83e5ee1b8ed53fd59176b770d
TyRoXx <tyroxxxx@gmail.com>
2013-06-08 15:09:18 +0200

    begin to serve files

--- tempest.cpp
+++ tempest.cpp
@@ -11,6 +11,7 @@
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/operations.hpp>
 #include <memory>
+#include <fstream>
 
 namespace tempest
 {
@@ -102,42 +103,133 @@ namespace tempest
 		begin_accept();
 	}
 
-	void handle_request_threaded(boost::shared_ptr<abstract_client> client)
+	boost::optional<boost::filesystem::path>
+	complete_served_path(boost::filesystem::path const &top,
+	                     std::string const &requested)
+	{
+		//TODO white-listing instead of black-listing
+		if (requested.find("..") != std::string::npos)
+		{
+			return boost::optional<boost::filesystem::path>();
+		}
+
+		return top / requested;
+	}
+
+	http_response make_bad_request()
+	{
+		http_response response;
+		response.version = "HTTP/1.1";
+		response.status = 400;
+		response.reason = "Bad Request";
+		response.headers["Content-Type"] = "text/html";
+		response.headers["Content-Length"] = "0";
+		return response;
+	}
+
+	http_response make_not_found_response(std::string const &requested_file)
+	{
+		http_response response;
+		response.version = "HTTP/1.1";
+		response.status = 404;
+		response.reason = "Not Found";
+		response.headers["Content-Type"] = "text/html";
+
+		response.body =
+		        "<h2>The requested file could not be found</h2>"
+		        "<p>" + requested_file + "</p>"
+		        ;
+
+		response.headers["Content-Length"] =
+		        boost::lexical_cast<std::string>(response.body.size());
+		return response;
+	}
+
+	http_response make_not_implemented_response(std::string const &requested_file)
+	{
+		http_response response;
+		response.version = "HTTP/1.1";
+		response.status = 501;
+		response.reason = "Not Implemented";
+		response.headers["Content-Type"] = "text/html";
+
+		response.body =
+		        "<h2>could not serve the file because some required "
+		        "functionality is not implemented in the web server</h2>"
+		        "<p>" + requested_file + "</p>"
+		        ;
+
+		response.headers["Content-Length"] =
+		        boost::lexical_cast<std::string>(response.body.size());
+		return response;
+	}
+
+	http_response make_file_response(std::istream &content,
+	                                 std::size_t file_size)
+	{
+		http_response response;
+		response.headers["Content-Length"] =
+		        boost::lexical_cast<std::string>(file_size);
+		response.status = 200;
+		response.reason = "OK";
+		response.version = "HTTP/1.1";
+		std::copy_n(std::istreambuf_iterator<char>(content),
+		            file_size,
+		            std::back_inserter(response.body));
+		return response;
+	}
+
+	http_response respond(http_request const &request,
+	                      boost::filesystem::path const &top)
+	{
+		if (request.method != "GET" &&
+		    request.method != "POST")
+		{
+			return make_bad_request();
+		}
+
+		boost::optional<boost::filesystem::path> const full_path =
+		        complete_served_path(top, request.file);
+
+		if (!full_path ||
+		        !boost::filesystem::is_regular(*full_path))
+		{
+			return make_not_found_response(request.file);
+		}
+
+		std::ifstream file(full_path->string(),
+		                   std::ios::binary);
+		if (!file)
+		{
+			return make_not_found_response(request.file);
+		}
+
+		file.exceptions(std::ios::failbit | std::ios::badbit | std::ios::eofbit);
+
+		auto const file_size = boost::filesystem::file_size(*full_path);
+		if (file_size > std::numeric_limits<std::size_t>::max())
+		{
+			return make_not_implemented_response(request.file);
+		}
+
+		return make_file_response(file, file_size);
+	}
+
+	void handle_request_threaded(boost::shared_ptr<abstract_client> client,
+	                             boost::filesystem::path const &directory)
 	{
 		//We have to catch exceptions before they propagate to
 		//boost::thread because that would terminate the whole process.
 		try
 		{
 			http_request request = parse_request(client->request());
-
-			http_response response;
-			response.version = "HTTP/1.1";
-			response.status = 200;
-			response.reason = "OK";
-			response.headers["Content-Type"] = "text/html";
-
-			response.body =
-			        "<h2>Hello, World!</h2>"
-			        "<p>" + request.method +
-			        " " + request.file +
-			        " " + request.version + "</p>"
-			        ;
-
-			response.body += "<ul>";
-			for (auto const &header : request.headers)
-			{
-				response.body += "<li>" + header.first + ": " + header.second;
-			}
-			response.body += "</ul>";
-
-			response.headers["Content-Length"] =
-			        boost::lexical_cast<std::string>(response.body.size());
+			http_response response = respond(request, directory);
 			print_response(response, client->response());
 			client->response().flush();
 		}
 		catch (std::exception const &ex)
 		{
-			//TODO
+			std::cerr << ex.what() << '\n';
 		}
 	}
 
@@ -145,7 +237,8 @@ namespace tempest
 	                   boost::filesystem::path const &directory)
 	{
 		boost::shared_ptr<abstract_client> const shared_client(client.release());
-		boost::thread client_thread(handle_request_threaded, shared_client);
+		boost::thread client_thread(handle_request_threaded,
+		                            shared_client, directory);
 		client_thread.detach();
 	}
 
